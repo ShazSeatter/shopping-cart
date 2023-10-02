c1e8ebf15ce638519a8a7b5e10e0ae7b19b3d193
Shazreen Seatter <shazseatter12@gmail.com>
2023-10-02 13:46:57 +0100

    image folder name change

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import './style.css'
 
+
 import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    <img src="assets/shopping-basket.png"/>
+    <img src="images/shopping-basket.png"/>
     <input id="input-field" type="text" placeholder="Enter a item..."/>    
     <button id="add-btn">Add to cart</button>
     <ul id="shopping-list">
